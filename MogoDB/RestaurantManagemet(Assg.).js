c186852c4449527b362129d9f16836def9762faf
Shubham Mahajan <srmhjn@gmail.com>
2018-06-26 20:07:18 +0530

    Add files via upload

---
+++ MogoDB/RestaurantManagemet(Assg.).js
@@ -0,0 +1,284 @@
+//----------------------------------Inserting Data---------------------------------------//
+db.restaurants.insert
+({
+    _id:1,
+    RestaurantId:"r001",
+    Name:"Kwality Foods",
+    Address:{
+            Street:"Allen Street",
+            City:"Mumbai",
+            Pincode:100001
+            },
+    Cuisine:["Indian",
+             "Chinese",
+             "Italian"
+            ],
+    District:"Thane",
+    Rating:[
+            {"Date": new Date("10/06/2018"),Rating:4.5,Scores:10},
+            {"Date": new Date("11/05/2018"),Rating:3,Scores:7},
+            {"Date": new Date("12/06/2018"),Rating:4,Scores:9},
+            {"Date": new Date("14/07/2018"),Rating:3.5,Scores:8}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:2,
+    RestaurantId:"r002",
+    Name:"Bhopal Foods",
+    Address:{
+            Street:"St. Cruz",
+            City:"Mumbai",
+            Pincode:100001
+            },
+    Cuisine:["Veg",
+             "Non-Veg"
+            ],
+    District:"Navi-Mumbai",
+    Rating:[
+            {"Date": new Date("11/05/2018"),Rating:4.5,Scores:10},
+            {"Date": new Date("10/04/2018"),Rating:3.5,Scores:8},
+            {"Date": new Date("11/07/2018"),Rating:4.5,Scores:9},
+            {"Date": new Date("14/08/2018"),Rating:3.5,Scores:9}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:3,
+    RestaurantId:"r003",
+    Name:"Indore Foods",
+    Address:{
+            Street:"Rajwada",
+            City:"Indore",
+            Pincode:400004
+            },
+    Cuisine:["Indian",
+             "Chinese",
+             "Italian",
+             "Veg",
+             "Non-Veg"
+            ],
+    District:"Indore",
+    Rating:[
+            {"Date": new Date("10/05/2018"),Rating:4.5,Scores:10},
+            {"Date": new Date("11/04/2018"),Rating:3,Scores:8},
+            {"Date": new Date("12/05/2018"),Rating:4.5,Scores:9},
+            {"Date": new Date("14/08/2018"),Rating:3.0,Scores:7}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:4,
+    RestaurantId:"r004",
+    Name:"Food Adda",
+    Address:{
+            Street:"Sunday Street",
+            City:"Mumbai",
+            Pincode:100001
+            },
+    Cuisine:["Indian",
+             "Chinese",
+             "Italian"
+            ],
+    District:"Vile-Parle",
+    Rating:[
+            {"Date": new Date("05/06/2018"),Rating:4,Scores:9},
+            {"Date": new Date("10/07/2018"),Rating:3,Scores:8},
+            {"Date": new Date("12/06/2018"),Rating:4,Scores:9},
+            {"Date": new Date("20/07/2018"),Rating:3,Scores:7}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:5,
+    RestaurantId:"r005",
+    Name:"Sangeetha",
+    Address:{
+            Street:"Porud",
+            City:"Chennai",
+            Pincode:600089
+            },
+    Cuisine:["Veg",
+             "Non-Veg"
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("13/07/2018"),Rating:4.5,Scores:10},
+            {"Date": new Date("10/05/2018"),Rating:4,Scores:8},
+            {"Date": new Date("05/05/2018"),Rating:4,Scores:9},
+            {"Date": new Date("04/06/2018"),Rating:3.5,Scores:8}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:6,
+    RestaurantId:"r006",
+    Name:"Srimithai",
+    Address:{
+            Street:"Egmore",
+            City:"Chennai",
+            Pincode:600089
+            },
+    Cuisine:["Veg",
+             "Non-Veg"
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("13/07/2018"),Rating:4.0,Scores:10},
+            {"Date": new Date("10/05/2018"),Rating:4,Scores:8},
+            {"Date": new Date("05/05/2018"),Rating:4,Scores:8},
+            {"Date": new Date("04/06/2018"),Rating:5,Scores:10}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:7,
+    RestaurantId:"r007",
+    Name:"Karthik",
+    Address:{
+            Street:"Marina",
+            City:"Chennai",
+            Pincode:600089
+            },
+    Cuisine:["Veg"
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("10/07/2018"),Rating:4.0,Scores:9},
+            {"Date": new Date("10/05/2018"),Rating:3,Scores:8},
+            {"Date": new Date("05/05/2018"),Rating:3,Scores:7},
+            {"Date": new Date("20/06/2018"),Rating:2,Scores:5}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:8,
+    RestaurantId:"r008",
+    Name:"Kaveri Foods",
+    Address:{
+            Street:"Egmore",
+            City:"Chennai",
+            Pincode:600089
+            },
+    Cuisine:["Veg",
+             "Non-Veg"
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("13/07/2018"),Rating:3.5,Scores:8},
+            {"Date": new Date("21/05/2018"),Rating:4,Scores:9},
+            {"Date": new Date("23/05/2018"),Rating:4.5,Scores:9},
+            {"Date": new Date("30/06/2018"),Rating:5,Scores:10}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:9,
+    RestaurantId:"r009",
+    Name:"Bank Foods",
+    Address:{
+            Street:"Yashwant",
+            City:"Banglore",
+            Pincode:600089
+            },
+    Cuisine:["Veg",
+             "Non-Veg",
+             "Chinese",
+             "Italian"
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("01/05/2018"),Rating:4,Scores:8},
+            {"Date": new Date("12/05/2018"),Rating:4,Scores:9},
+            {"Date": new Date("05/07/2018"),Rating:4,Scores:9},
+            {"Date": new Date("27/06/2018"),Rating:5,Scores:10}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:10,
+    RestaurantId:"r010",
+    Name:"TDS",
+    Address:{
+            Street:"MP nagar",
+            City:"Bhopal",
+            Pincode:400003
+            },
+    Cuisine:["Veg",
+             "Non-Veg",
+             "Chinese",
+             "Italian"
+            ],
+    District:"Bhopal",
+    Rating:[
+            {"Date": new Date("07/05/2018"),Rating:4.5,Scores:8},
+            {"Date": new Date("13/05/2018"),Rating:3.5,Scores:7},
+            {"Date": new Date("04/04/2018"),Rating:4.5,Scores:9},
+            {"Date": new Date("02/06/2018"),Rating:5,Scores:10}
+           ]
+})
+
+db.restaurants.insert
+({
+    _id:11,
+    RestaurantId:"r011",
+    Name:"downtown",
+    Address:{
+            Street:"Anna Nagar",
+            City:"Chennai",
+            Pincode:600089
+            },
+    Cuisine:["Veg",
+             "Non-Veg",
+            ],
+    District:"Chennai",
+    Rating:[
+            {"Date": new Date("17/05/2018"),Rating:4.5,Scores:8},
+            {"Date": new Date("23/05/2018"),Rating:3.5,Scores:7},
+            {"Date": new Date("24/04/2018"),Rating:5,Scores:10},
+            {"Date": new Date("12/06/2018"),Rating:5,Scores:10}
+           ]
+})
+//1. Write a MongoDB query to display all the documents in the collection restaurants.  
+db.restaurants.find();
+---------------------------------------------------------------------------------------------
+//2.Write a MongoDB query to display the fields restaurant_id, name, and cuisine for all 
+//  the documentsin the collection restaurant.
+db.restaurants.find({},{Name:1,Cuisine:1,RestaurantId:1})
+---------------------------------------------------------------------------------------------
+
+//3. Write a MongoDB query to display the fields restaurant_id, name, and cuisine, but 
+//   exclude the field _id for all the documents in the collection restaurant. 
+db.restaurants.find({},{Name:1,Cuisine:1,RestaurantId:1,_id:0})
+----------------------------------------------------------------------------------------------
+//4. Write a MongoDB query to display all the restaurant which is in Chennai.
+db.restaurants.find({"Address.City":"Chennai"},{_id:0})
+-----------------------------------------------------------------------------------------------
+//5. Write a MongoDB query to display the first 3 restaurant which is in the district 
+//   Chennai.
+db.restaurants.find({"Address.City":"Chennai"},{_id:0}).limit(3)
+-----------------------------------------------------------------------------------------------
+//6.Write a MongoDB query to display the next 3 restaurants after skipping first 3 which
+//  are in the Chennai.
+db.restaurants.find({"Address.City":"Chennai"},{_id:0}).skip(3).limit(3)
+-----------------------------------------------------------------------------------------------
+//7. Write a MongoDB query to find the restaurants who achieved a score more than 7
+db.restaurants.find({"Rating.Scores":{$gt:7}},{_id:0})
+-----------------------------------------------------------------------------------------------
+//8. Write a MongoDB query to find the restaurants that achieved a score, more than 
+//    7 but less than 9. 
+db.restaurants.find({"Rating.Scores":{$gt:7,$lt:9}},{_id:0})
+-----------------------------------------------------------------------------------------------
+//9.Write a MongoDB query to find the restaurants that do not prepare any cuisine 
+//   of “Veg” and their grade score more than 7.
+db.restaurants.find({Cuisine:{$nin:["Veg"]}},{$gt:7})
+-----------------------------------------------------------------------------------------------
---
+++ MogoDB/bankmanagement(Eval.).js
@@ -0,0 +1,120 @@
+//using a databse
+use BankManagement
+--------------------------------------------------------------------------------------------------------------------
+//creating collection
+db.createCollection("bankdata")  
+-----------------------------------------------------------------------------------------------------------------------------
+//Inserting Entries
+//sample records:
+//1.
+db.bankdata.insert({
+    _id : "a14",
+    accountNo : "1014",
+    balance : 15000,
+    customerDetails:{
+        customerId : "c14",
+        customerName : "Bharat Omar",
+        street : "rockHills",
+        city : "Hyderabad",
+        loanAmount : 10000,
+        depositAmount : 2000
+    },
+    branchDetails : {
+        "branchName" : "Popular Market",
+        "branchCity" : "Hyderabad",
+        "assets" : 10000000.0
+    }
+})
+//2.
+db.bankdata.insert({
+    _id : "a15",
+    accountNo : "1015",
+    balance : 5200,
+    customerDetails:{
+        customerId : "c15",
+        customerName : "Ravi Arora",
+        street : "Shyamla",
+        city : "chennai",
+        loanAmount : 0,
+        depositAmount : 0
+    },
+    branchDetails : {
+        "branchName" : "Raapuram",
+        "branchCity" : "Chennai",
+        "assets" : 8000000.0
+    }
+})
+-----------------------------------------------------------------------------------------------------
+//fetching Records    
+db.bankdata.find()
+------------------------------------------------------------------------------------------------- 
+//Creating accountNo as Index and unique Key
+db.bankdata.ensureIndex({accountNo:1},{unique:true})
+--------------------------------------------------------------------------------------------------
+//Display Names and Cities of all the Borrowers
+db.bankdata.find({"customerDetails.loanAmount":{$gt:0}},
+                 {"customerDetails.customerName":1,"customerDetails.city":1,_id:0})
+-----------------------------------------------------------------------------------------------------
+//The names of borrowers who live in Chennai
+db.bankdata.find({"customerDetails.loanAmount":{$gt:0},"customerDetails.city":"Chennai"},
+                 {"customerDetails.customerName":1})
+----------------------------------------------------------------------------------------------------------
+//Find the name, city, and assets of the branch with the largest assets
+                       
+db.bankdata.find({},{"branchDetails.branchName":1,
+                     "branchDetails.branchCity":1,
+                     "branchDetails.assets":1})
+           .sort({"branchDetails.assets":-1})
+           .limit(1)                        
+                                              
+------------------------------------------------------------------------------------------------        
+//The names and cities of customers who have a loan at Pune branch.  
+db.bankdata.find({"customerDetails.loanAmount":{$gt:0},
+                  "branchDetails.branchCity":"Pune"},
+                 {"customerDetails.customerName":1,
+                  "customerDetails.city":1}) 
+------------------------------------------------------------------------------------------------
+//Find the number of accounts with balances between 700 and 900.
+db.bankdata.aggregate([{$match:{balance:{$gt:700,$lt:900}}},{$count:"Account_No"}])           
+---------------------------------------------------------------------------------------------
+//The names of customers with both deposits and loans at Pune branch.
+db.bankdata.find({"customerDetails.loanAmount":{$gt:0},
+                  "customerDetails.depositAmount":{$gt:0},
+                  "branchDetails.branchCity":"Pune"},
+                  {"customerDetails.customerName":1})
+----------------------------------------------------------------------------------------------                 
+//The customers whose total loans are greater than the total amount in their bank accounts
+
+db.bankdata.find({ $where: "this.customerDetails.loanAmount > this.balance"},
+                  {"customerDetails.customerName":1,accountNo:1,
+                   "customerDetails.loanAmount":1,balance:1})
+------------------------------------------------------------------------------------------------                                              
+//The names of customers living on streets with names ending in "Hill".
+db.bankdata.find({"customerDetails.street":{$regex:"Hills$"}},
+                 {"customerDetails.customerName":1}) 
+---------------------------------------------------------------------------------------------   
+//The names of customers with an account but not a loan at Pune branch                  
+db.bankdata.find({"branchDetails.branchCity":"Pune","customerDetails.loanAmount":0},
+                 {"customerDetails.customerName":1,accountNo:1}) 
+-----------------------------------------------------------------------------------------------                
+//The branch with the largest average balance
+db.bankdata.aggregate([{
+ $group :{ _id :"$branchDetails.branchName",AvgBalance:{$avg:"$balance"}}}
+ ,{$sort:{"AvgBalance":-1}},{$limit:1}]) 
+
+--------------------------------------------------------------------------------------------------------====---------
+//The branch name and number of customers for each branch 
+db.bankdata.aggregate([{$group:{_id:"$branchDetails.branchName",totalCustomers:{$sum:1}}}]) 
+  
+-----------------------------------------------------------------------------------------------------------------------------------------
+//Deposit an additional Rs. 20,000 to Ram’s bank account. 
+ 
+db.bankdata.update({"customerDetails.customerName":"Ram Sharma"},{$inc:{balance:20000}})
+
+----------------------------------------------------------------------------------------------------------------------------                                   
+------------------------------------------********------------------------------------------------------------------------------------------                    
+     
+     
+     
+                  
+                 
